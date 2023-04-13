e36674d09982cc32f70eb686988e03812acdcdb3
Pavel Vdovenko <reactivecake@gmail.com>
2023-04-13 22:06:54 +0400

    refactor(Layout): move to a separated file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,6 @@
-import Cookies from 'js-cookie'
+import { Routes, Route, Link } from 'react-router-dom'
 
-import {
-  Routes,
-  Route,
-  Outlet,
-  Link,
-  Navigate,
-  useLocation,
-} from 'react-router-dom'
-
-import { Container } from 'react-bootstrap'
-import { Nav } from 'react-bootstrap'
-import { Navbar } from 'react-bootstrap'
-// import { NavDropdown } from 'react-bootstrap';
-
+import Layout from './Layout'
 import Home from './Home'
 import Dashboard from './Dashboard'
 import Login from './Login'
@@ -39,52 +26,6 @@ const App = () => {
         <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
-  )
-}
-
-const Layout = () => {
-  const location = useLocation()
-  const accessToken = Cookies.get('access_token')
-
-  if (
-    !accessToken &&
-    location.pathname !== '/login' &&
-    location.pathname !== '/register'
-  ) {
-    return <Navigate to="/login" />
-  }
-
-  return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">📈 Sales App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              {/* <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown> */}
-            </Nav>
-            <Nav>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/register">Register</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Outlet />
-    </div>
   )
 }
 
